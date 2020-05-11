42328433717c0062c1b1351f433c9bd0c252ad3a
dasha akhten <dasha.akhten@gmail.com>
2020-05-11 12:15:52 -0700

    starts crying

--- HashMap_drills.js
+++ HashMap_drills.js
@@ -1,28 +1,27 @@
-const HashMap = require('./hashmap');
+const HashMap = require("./hashmap");
 
 function main() {
   let lotr = new HashMap();
 
-  lotr.set('Hobbit', 'Bilbo');
-  lotr.set('Hobbit', 'Frodo');
-  lotr.set('Wizard', 'Gandalf');
-  lotr.set('Human', 'Aragorn');
-  lotr.set('Elf', 'Legolas');
-  lotr.set('Maiar', 'The Necromancer');
-  lotr.set('Maiar', 'Sauron');
-  lotr.set('RingBearer', 'Gollum');
-  lotr.set('LadyOfLight', 'Galadriel');
-  lotr.set('HalfElven', 'Arwen');
-  lotr.set('Ent', 'Treebeard');
+  lotr.set("Hobbit", "Bilbo");
+  lotr.set("Hobbit", "Frodo");
+  lotr.set("Wizard", "Gandalf");
+  lotr.set("Human", "Aragorn");
+  lotr.set("Elf", "Legolas");
+  lotr.set("Maiar", "The Necromancer");
+  lotr.set("Maiar", "Sauron");
+  lotr.set("RingBearer", "Gollum");
+  lotr.set("LadyOfLight", "Galadriel");
+  lotr.set("HalfElven", "Arwen");
+  lotr.set("Ent", "Treebeard");
 
-  console.log(lotr.get('Hobbit'));
-  console.log(lotr.get('Maiar'));
+  console.log(lotr.get("Hobbit"));
+  console.log(lotr.get("Maiar"));
 
   return lotr;
 }
 
 console.log(main());
-
 
 // Drill 1.Create a Hash Map class
 // *Returned Sauron and Frodo from the "Maiar" and "Hobbit" keys. the hash seems to be rewriting the value when it finds an identical key.
@@ -32,22 +31,39 @@ console.log(main());
 // map1.get(str1) = 20
 // map2.get(str3) = 10
 
-const WhatDoesThisDo = function(){
-  let str1 = 'Hello World.';
-  let str2 = 'Hello World.';
+const WhatDoesThisDo = function () {
+  let str1 = "Hello World.";
+  let str2 = "Hello World.";
   let map1 = new HashMap();
-  map1.set(str1,10);
-  map1.set(str2,20);
+  map1.set(str1, 10);
+  map1.set(str2, 20);
   let map2 = new HashMap();
   let str3 = str1;
   let str4 = str2;
-  map2.set(str3,20);
-  map2.set(str4,10);
+  map2.set(str3, 20);
+  map2.set(str4, 10);
 
   console.log(map1.get(str1));
   console.log(map2.get(str3));
-}
+};
 
 // WhatDoesThisDo();
 
-// 
+// question 4
+function duplicate(string) {
+  let hashmap = new HashMap();
+  for (let i = 0; i < string.length; i++) {
+    hashmap.set(string.charAt(i), string.charAt(i));
+  }
+  let newString = "";
+  for (let i = 0; i <= hashmap._capacity; i++) {
+    // console.log(i);
+    if (!hashmap._hashTable[i]) {
+    } else {
+      newString += hashmap._hashTable[i].value;
+    }
+  }
+  console.log(hashmap);
+  return newString;
+}
+console.log(duplicate("google all that you think can think of"));
